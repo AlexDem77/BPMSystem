b8c72794ca94fb032f44973f34ffd7150df8bad0
AlexDem <demdem300@yandex.ru>
2021-08-04 22:27:53 +0300

    2021-08-04-rev02. Добавить поля в сущности
    Signed-off-by: AlexDem

--- BPMApp/BPM.Db/Department.cs
+++ BPMApp/BPM.Db/Department.cs
@@ -20,5 +20,15 @@ namespace BPMApp.Db
         /// Наименование отдела
         /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Начальник отдела
+        /// </summary>
+        public string DepartmentHead { get; set; }
+
+        /// <summary>
+        /// Внутренний номер
+        /// </summary>
+        public int ExtensionNumber { get; set; }
     }
 }
