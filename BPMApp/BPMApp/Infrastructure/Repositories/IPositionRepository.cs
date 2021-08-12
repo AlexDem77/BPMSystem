830ed7ad5f05fcee239035f8f2cc22ad28423d4f
AlexDem <demdem600@yandex.ru>
2021-08-12 07:56:54 +0300

    2021-08-12-rev03. Обновить зависимости
    Signed-off-by: AlexDem
    
    - Добавить IPositionRepository.cs
    - Реализовать IPositionRepository.cs
    - Добавить контроллер PositionController.cs

---
+++ BPMApp/BPMApp/Infrastructure/Repositories/IPositionRepository.cs
@@ -0,0 +1,24 @@
+﻿using BpmApp.Models.PositionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BpmApp.Infrastructure.Repositories
+{
+    interface IPositionRepository
+    {
+        /// <summary>
+        /// Получение всех должностей
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<PositionModel> GetAll();
+
+        /// <summary>
+        /// Поиск или создание должности в контексте
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        PositionModel Upsert(CreatePositionModel model);
+    }
+}
---
+++ BPMApp/BPMApp/Infrastructure/Repositories/PositionRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BpmApp.Models.PositionModels;
+using BpmApp.Db.Infrastructure;
+using BpmApp.Db.Entities;
+
+namespace BpmApp.Infrastructure.Repositories
+{
+    public class PositionRepository : IPositionRepository
+    {
+        private readonly BpmContext _bpmContext;
+
+        public PositionRepository(BpmContext bpmContext)
+        {
+            _bpmContext = bpmContext;
+        }
+
+        public IEnumerable<PositionModel> GetAll()
+        {
+            var result = _bpmContext
+                .Positions
+                .Select(p => new PositionModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Title = p.Title
+                })
+                .ToList();
+
+            return result;
+        }
+
+        public PositionModel Upsert(CreatePositionModel model)
+        {
+            var position = _bpmContext.Positions.SingleOrDefault(p => p.Name == model.Name && p.Title == model.Title);
+            if (position is null)
+            {
+                position = new Position
+                {
+                    Name = model.Name,
+                    Title = model.Title
+                };
+                _bpmContext.Positions.Add(position);
+                _bpmContext.SaveChanges();
+            }
+
+            return new PositionModel
+            {
+                Id = position.Id,
+                Name = position.Name,
+                Title = position.Title
+            };
+        }
+    }
+}
