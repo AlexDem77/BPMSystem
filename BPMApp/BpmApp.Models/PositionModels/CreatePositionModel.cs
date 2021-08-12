54a8cd9edc3bb7ad5174652d0da3834cb8d28ca1
AlexDem <demdem600@yandex.ru>
2021-08-12 07:43:40 +0300

    2021-08-12-rev02. Добавить DTO-модели для Position
    Signed-off-by: AlexDem

---
+++ BPMApp/BpmApp.Models/PositionModels/CreatePositionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpmApp.Models.PositionModels
+{
+    /// <summary>
+    /// DTO-модель должности (создание)
+    /// </summary>
+    public class CreatePositionModel
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ BPMApp/BpmApp.Models/PositionModels/PositionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpmApp.Models.PositionModels
+{
+    /// <summary>
+    /// DTO-модель должности (отображение)
+    /// </summary>
+    public class PositionModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
